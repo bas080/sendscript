ed1dee819277e75af14bd55ae61290c88961a12a
bas081 <bas080@hotmail.com>
2023-09-14 22:48:05 +0200

    Make DSL node an object with toJSON method

--- dsl.mjs
+++ dsl.mjs
@@ -9,16 +9,17 @@ export default function dsl (schema, call) {
     const then = (program) => (resolve, reject) =>
       Promise.resolve(call(program)).then(resolve, reject)
 
-    const fn = (...args) => {
-      // Only await non dsl values
-      const v = [fn, ...args]
-
-      v[symbol] = symbol
-      v.then = async (...thenArgs) => {
-        return then(await promiseOnlyNonDSL(v))(...thenArgs)
+    const fn = (...args) => ({
+      [symbol]: symbol,
+      toJSON () {
+        return [fn, ...args]
+      },
+      async then (resolve, reject) {
+        return then(await promiseOnlyNonDSL([
+          fn, ...args
+        ]))(resolve, reject)
       }
-      return v
-    }
+    })
 
     fn.toJSON = () => ['ref', name]
     fn.then = then(fn)
