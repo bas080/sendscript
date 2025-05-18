36bb5022407a7540696787182984c3c641437c1d
bas080 <bas080@hotmail.com>
2025-05-18 07:55:24 -0400

    fixup! Add cli with repl command

--- cli.mjs
+++ cli.mjs
@@ -1,24 +1,15 @@
 #!/usr/bin/env node
 
-import { program } from 'commander'
 import path from 'path'
 import { pathToFileURL } from 'url'
 import stringify from './stringify.mjs'
 import repl from './repl.mjs'
 import Parse from './parse.mjs'
 
-program
+  const modulePath = pathToFileURL(path.resolve(process.cwd(), process.argv[2])).href
+  const mod = await import(modulePath)
+  const parse = Parse(mod)
 
-  .command('repl <module>')
-  .description('Start a repl that uses sendscript parsing')
-  .action(async (module, options) => {
-    const modulePath = pathToFileURL(path.resolve(process.cwd(), module)).href
-    const mod = await import(modulePath)
-    const parse = Parse(mod)
+  const send = program => parse(stringify(program))
 
-    const send = program => parse(stringify(program))
-
-    repl(send, mod)
-  })
-
-program.parse(process.argv)
+  repl(send, mod)
