f0beb27129b2ea758c0d96105f27e30db88e5a85
bas080 <bas080@hotmail.com>
2026-04-09 09:23:39 +0300

    Fix the cli repl
    Got broken during refactor to v2

--- cli.mjs
+++ cli.mjs
@@ -2,13 +2,14 @@
 
 import path from 'path'
 import { pathToFileURL } from 'url'
-import stringify from './stringify.mjs'
+import Stringify from './stringify.mjs'
 import repl from './repl.mjs'
 import Parse from './parse.mjs'
 
+const stringify = Stringify()
 const modulePath = pathToFileURL(path.resolve(process.cwd(), process.argv[2])).href
 const mod = await import(modulePath)
-const parse = Parse(mod)
+const parse = Parse(Object.keys(mod), mod)
 
 const send = program => parse(stringify(program))
 
--- repl.mjs
+++ repl.mjs
@@ -1,8 +1,8 @@
-import instrument from './module.mjs'
+import references from './references.mjs'
 import repl from 'node:repl'
 
 export default async function sendscriptRepl (send, module) {
-  Object.assign(global, instrument(module))
+  Object.assign(global, references(Object.keys(module)))
 
   async function cb (cmd, context, filename, callback) {
     callback(null, await send(eval(cmd))) // eslint-disable-line no-eval
